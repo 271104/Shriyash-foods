33a490e2aff4c8248b6ebe68f0bd3c73be5d7240
Shubham Charate <shubhamcharate02@gmail.com>
2026-06-04 16:52:10 +0530

    Added Shiprockett

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -5,7 +5,7 @@
 
 const API_BASE =
   process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_URL || 'https://shriyash-foods.onrender.com/api'
+    ? 'https://shriyash-foods.onrender.com/api'
     : process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 export default API_BASE;
